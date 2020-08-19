469f25050a4a4250d26ee069a82ee23b34c36850
Leonardo Ibarra <ibarrapalacios@gmail.com>
2020-08-19 10:07:37 -0500

    Creación de nuevas funciones

--- funciones.py
+++ funciones.py
@@ -6,18 +6,26 @@
 #imprimir_mensaje()
 #imprimir_mensaje()
 
-def conversasion(mensaje):
-    print("Hola")
-    print("Cómo estás")
-    print(mensaje)
-    print("Adios")
+# def conversasion(mensaje):
+#     print("Hola")
+#     print("Cómo estás")
+#     print(mensaje)
+#     print("Adios")
 
-opcion = int(input("Elige una opción (1, 2, 3)"))
-if opcion == 1:
-    conversasion("Elegiste la opción 1")
-elif opcion == 2:
-    conversasion("Elegiste la opción 2")
-elif opcion == 3:
-    conversasion("Elegiste la opción 1")
-else:
-    print("Escribe la opción correcta")
+# opcion = int(input("Elige una opción (1, 2, 3)"))
+# if opcion == 1:
+#     conversasion("Elegiste la opción 1")
+# elif opcion == 2:
+#     conversasion("Elegiste la opción 2")
+# elif opcion == 3:
+#     conversasion("Elegiste la opción 1")
+# else:
+#     print("Escribe la opción correcta")
+
+def suma(a, b):
+    print("Se suman dos numeros")
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
