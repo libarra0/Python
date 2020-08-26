7007c44a0377983c0281d61b6ff7b79d8d659b7d
Leonardo Ibarra <ibarrapalacios@gmail.com>
2020-08-26 10:42:18 -0500

    Ciclo for

---
+++ for.py
@@ -0,0 +1,17 @@
+#While
+# contador = 1
+# print(contador)
+# while contador < 1000:
+#     contador += 1
+#     print(contador)
+
+# Listas
+#a = list(range(1000))
+#print(a)
+
+#Ciclo for con range()
+#for contador in range(1, 1001):
+#    print(contador)
+
+for i in range(10):
+    print(11*i)
