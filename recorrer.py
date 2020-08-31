05ea70d6973878e6d66f51d733a9d213e6571415
Leonardo Ibarra <ibarrapalacios@gmail.com>
2020-08-31 12:22:58 -0500

    Ciclo for

---
+++ recorrer.py
@@ -0,0 +1,10 @@
+def run():
+    # your_name = input('Write your name: ')
+    # for letter in your_name:
+    #     print(letter)
+    phrase = input('Write a phrase: ')
+    for character in phrase:
+        print(character.upper())
+
+if __name__ == "__main__":
+    run()
